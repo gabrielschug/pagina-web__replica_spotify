1097db746f5ffa543032d880bb62cc34963e2745
Gabriel Schug <gabrielschug@hotmail.com>
2025-04-27 02:57:26 -0300

    Primeiro commit com os arquivos iniciais

--- scripts.js
+++ scripts.js
@@ -1,30 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const artistData = [
-        {name: 'Henrique e Juliano', image: './img/artista-henrique-juliano.jpg'},
-        {name: 'Jorge e Mateus', image: './img/artista-jorge-mateus.jpg'},
-        {name: 'The Weeknd', image: './img/the-weekend.jpg'},
-        {name: 'Matue', image: './img/matue.jpg'},
-        {name: 'Zé Neto & Cristiano', image: './img/artista-ze-neto.jpg'},
-        {name: 'Eminem', image: './img/eminem.jpg'},
-        {name: 'Gusttavo Lima', image: './img/artista-gustavo-limma.jpg'},
-        {name: 'Luan Santana', image: './img/artista-luan-santana.jpg'},
-        {name: 'Grupo Menos é Mais', image: './img/menos.jpg'},
-
-        {name: 'Matheus e Kauan', image: './img/artista-mateus-kauan.jpg'},
+        {name: 'Henrique e Juliano', image: './assets/artista-henrique-juliano.jpg'},
+        {name: 'Jorge e Mateus', image: './assets/artista-jorge-mateus.jpg'},
+        {name: 'The Weeknd', image: './assets/the-weekend.jpg'},
+        {name: 'Matue', image: './assets/matue.jpg'},
+        {name: 'Zé Neto & Cristiano', image: './assets/artista-ze-neto.jpg'},
+        {name: 'Eminem', image: './assets/eminem.jpg'},
+        {name: 'Gusttavo Lima', image: './assets/artista-gustavo-limma.jpg'},
+        {name: 'Luan Santana', image: './assets/artista-luan-santana.jpg'},
+        {name: 'Grupo Menos é Mais', image: './assets/menos.jpg'},
+        {name: 'Matheus e Kauan', image: './assets/artista-mateus-kauan.jpg'},
     ]
     
     const albumsData = [
-        { name:'White Noise(Sleep & Relaxation Sounds',artist:'Sleppy John', image:'./img/album-white-noise.jpg'},
-        { name: 'YES OR NO', artist: 'TETO', image: './img/teto.jpg'},
-        { name: 'O céu Explica Tudo (Ao Vivo)', artist: 'Henrique e Juliano', image: './img/album-ceu-explica.jpg'},
-        { name: 'Nada como um dia após o outro', artist: 'Racionais', image: `img/vida-loka.jpg`},
-        { name: 'Decretos Reais', artist: 'Marília Mendonça', image: `img/marilia.jpg`},
-        { name: 'HIT ME HARD AND SOFT', artist: 'Billie Eilish', image: 'img/hit-me.jpg'},
-        { name: 'CAJU', artist: 'Liniker', image: 'img/album-caju.jpg'},
-        { name: 'MAYHEM', artist: 'Lady Gaga', image: 'img/gaga.jpg'},
-        { name: 'Escândalo Intimo', artist: 'Luísa Sonza', image: 'img/escandalo.jpg'},
-        { name: 'Check-in', artist: 'Jorge e Mateus', image: 'img/jorge.jpg'},
+        { name:'White Noise(Sleep & Relaxation Sounds',artist:'Sleppy John', image:'./assets/album-white-noise.jpg'},
+        { name: 'YES OR NO', artist: 'TETO', image: './assets/teto.jpg'},
+        { name: 'O céu Explica Tudo (Ao Vivo)', artist: 'Henrique e Juliano', image: './assets/album-ceu-explica.jpg'},
+        { name: 'Nada como um dia após o outro', artist: 'Racionais', image: `assets/vida-loka.jpg`},
+        { name: 'Decretos Reais', artist: 'Marília Mendonça', image: `assets/marilia.jpg`},
+        { name: 'HIT ME HARD AND SOFT', artist: 'Billie Eilish', image: 'assets/hit-me.jpg'},
+        { name: 'CAJU', artist: 'Liniker', image: 'assets/album-caju.jpg'},
+        { name: 'MAYHEM', artist: 'Lady Gaga', image: 'assets/gaga.jpg'},
+        { name: 'Escândalo Intimo', artist: 'Luísa Sonza', image: 'assets/escandalo.jpg'},
+        { name: 'Check-in', artist: 'Jorge e Mateus', image: 'assets/jorge.jpg'},
     ]
 
     const artistsGrid = document.querySelector('.artists__grid')
